1c3e331196b4f748da204bf7a591ec1f0bcf4f21
Akashkayande <ramdaskayande637@gmail.com>
2025-09-14 07:38:34 +0530

    add logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   const {authUser} = useContext(AuthContext)
   return (
     <div className="bg-[url('/bgImage.svg')] bg-contain">
-      <Toaster/
+      <Toaster/>
       <Routes>
         <Route path='/' element={authUser ? <HomePage/> : <Navigate to="/login" />} />
         <Route path='/login' element={ !authUser ? <LoginPage/> : <Navigate to="/" />} />
